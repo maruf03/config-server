6cdee937d4daff2ccc7e74b83836239b138c7ee1
maruf03 <maruf.alif03@gmail.com>
2024-08-13 13:33:12 +0600

    first commit

---
+++ src/test/java/com/dohatec/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dohatec.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
